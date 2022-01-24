e6f457bac452c641a2cc9d199cc35c9828ebbe53
AnaG0307 <ana.genover@gmail.com>
2022-01-24 12:09:21 +0000

    Added Stripe element

--- checkout/static/checkout/js/stripe_elements.js
+++ checkout/static/checkout/js/stripe_elements.js
@@ -1,8 +1,7 @@
 /*
-    Core logic/payment logic flow for this comes from here:
+    Core logic/payment flow for this comes from here:
     https://stripe.com/docs/payments/accept-a-payment
-
-    CSS from here:
+    CSS from here: 
     https://stripe.com/docs/stripe-js
 */
 
@@ -10,15 +9,14 @@ var stripe_public_key = $('#id_stripe_public_key').text().slice(1, -1);
 var client_secret = $('#id_client_secret').text().slice(1, -1);
 var stripe = Stripe(stripe_public_key);
 var elements = stripe.elements();
-var card = elements.create('card');
-
 var style = {
     base: {
         color: '#000',
         fontFamily: '"Helvetica Neue", Helvetica, sans-serif',
-        fontSmoothing: '16px',
+        fontSmoothing: 'antialiased',
+        fontSize: '16px',
         '::placeholder': {
-            color: '#aab7c4',
+            color: '#aab7c4'
         }
     },
     invalid: {
@@ -26,5 +24,5 @@ var style = {
         iconColor: '#dc3545'
     }
 };
-
-card.mount('#card-element', {style: style});
+var card = elements.create('card', {style: style});
+card.mount('#card-element');
--- checkout/views.py
+++ checkout/views.py
@@ -3,21 +3,19 @@ from django.contrib import messages
 
 from .forms import OrderForm
 
+
 def checkout(request):
     bag = request.session.get('bag', {})
     if not bag:
         messages.error(request, "There is nothing in your bag at the moment")
         return redirect(reverse('products'))
-    
+
     order_form = OrderForm()
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
-        'stripe_public_key': 'pk_test_51KLODTJOqqWabdNEYK0oxfFJxycAOkKxg1oqpCC43Ye1MAFoAPrV3T4rDKGi6jlplg67tF0qW2onYatNbE3FfZJP00R37e3iGP',
+        'stripe_public_key': 'pk_test_0SMREd7Vdweb1MGRi8S0EycR00JVzSAs5O',
         'client_secret': 'test client secret',
     }
-    
+
     return render(request, template, context)
-
-
-
