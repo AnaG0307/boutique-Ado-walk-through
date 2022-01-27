bbe0b386c9703051cc86faadb73e69023fd08b75
AnaG0307 <ana.genover@gmail.com>
2022-01-27 12:34:38 +0000

    server not running

--- checkout/admin.py
+++ checkout/admin.py
@@ -12,7 +12,8 @@ class OrderAdmin(admin.ModelAdmin):
 
     readonly_fields = ('order_number','date',
                        'delivery_cost', 'order_total',
-                       'grand_total',)
+                       'grand_total', 'original_bag',
+                       'stripe_id')
 
     fields = ('order_number', 'date', 'full_name',
               'email', 'phone_number', 'country',
@@ -22,7 +23,8 @@ class OrderAdmin(admin.ModelAdmin):
     
     list_display = ('order_number', 'date', 'full_name',
                     'order_total', 'delivery_cost',
-                    'grand_total',)
+                    'grand_total', 'original_bag',
+                    'stripe_id')
     
     ordering = ('-date',)
 
