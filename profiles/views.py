350a43303cf30f331ff9e8a409db2c6de0342607
AnaG0307 <ana.genover@gmail.com>
2022-02-03 16:35:21 +0000

    Securing the views to prevent no superusers to access superuser pages

--- profiles/views.py
+++ profiles/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, get_object_or_404
+from django.contrib.auth.decorators import login_required
 from django.contrib import messages
 
 from .models import UserProfile
@@ -7,10 +8,11 @@ from .forms import UserProfileForm
 from checkout.models import Order
 
 
+@login_required
 def profile(request):
     """ Display the user's profile """
     profile = get_object_or_404(UserProfile, user=request.user)
-    
+
     if request.method == 'POST':
         form = UserProfileForm(request.POST, instance=profile)
         if form.is_valid():
