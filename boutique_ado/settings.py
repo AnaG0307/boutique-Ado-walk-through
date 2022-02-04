482ae7bb399936182a4729452c2a2494deccb029
AnaG0307 <ana.genover@gmail.com>
2022-02-04 12:44:14 +0000

    Deployment to AWS and set all the settings

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -52,6 +52,7 @@ INSTALLED_APPS = [
     # Others
     'crispy_forms',
     'django_countries',
+    'storages',
 ]
 
 MIDDLEWARE = [
@@ -175,6 +176,24 @@ STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+if 'USE_WAS' in os.environ:
+    #  Bucket config
+    AWS_STORAGE_BUCKET_NAME = 'boutique-ado-ana'
+    AWS_S3_REGION_NAME = 'Europe'
+    AWS_ACCESS_KEY_ID = os.environ.get(AWS_ACCESS_KEY_ID)
+    AWS_ACCESS_SECRET_KEY = os.environ.get(AWS_ACCESS_SECRET_KEY)
+    AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
+
+    # Static and Media Files
+    STATICFILES_STORAGE = 'custom_storages.StaticStorage'
+    STATICFILES_LOCATION = 'static'
+    DEFAULT_FILE_STORAGE = 'custom_storages.MediaStorage'
+    MEDIAFILES_LOCATION = 'media'
+
+    # Overrida static and media URLs in production
+    STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{STATICFILES_LOCATION}/'
+    MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{MEDIAFILES_LOCATION}/'
 
 
 # Stripe
