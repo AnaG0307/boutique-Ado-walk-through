568159cad3da83dd2a448e350773a945e635bd62
AnaG0307 <ana.genover@gmail.com>
2022-02-04 17:20:54 +0000

    Added cache control to settings.py

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -178,6 +178,12 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 if 'USE_AWS' in os.environ:
+    # Cache control
+    AWS_S3_OBJECT_PARAMETERS = {
+        'Expires': 'Thu, 31 Dec 2099 20:00 GMt',
+        'CacheControl': 'max-age=94608000',
+    }
+
     #  Bucket config
     AWS_STORAGE_BUCKET_NAME = 'boutique-ado-ana'
     AWS_S3_REGION_NAME = 'eu-west-2'
