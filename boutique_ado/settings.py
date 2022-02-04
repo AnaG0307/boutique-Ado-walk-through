7bef48bbb177483c2f41a1f997c48cd242fc6693
AnaG0307 <ana.genover@gmail.com>
2022-02-04 13:06:47 +0000

    Corrected typos in settings.py

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -181,8 +181,8 @@ if 'USE_AWS' in os.environ:
     #  Bucket config
     AWS_STORAGE_BUCKET_NAME = 'boutique-ado-ana'
     AWS_S3_REGION_NAME = 'eu-west-2'
-    AWS_ACCESS_KEY_ID = os.environ.get(AWS_ACCESS_KEY_ID)
-    AWS_SECRET_ACCESS_KEY = os.environ.get(AWS_SECRET_ACCESS_KEY)
+    AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
+    AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
     AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
 
     # Static and Media Files
